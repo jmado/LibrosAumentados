e4493dc384dc612f0862625a5150daedb38b3ec6
jmado <josemaldogomez@gmail.com>
2019-12-12 13:57:34 +0100

    Funciona
    archivos ignorados

--- LibrosAumentadosApp/resources/views/welcome.blade.php
+++ LibrosAumentadosApp/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Esto funciona
                 </div>
 
                 <div class="links">
