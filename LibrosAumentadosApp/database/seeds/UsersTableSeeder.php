92a07b4f7eb615b7bc06af148e1fda48e4782651
Adrian Sanchez Gil <adsagil@gmail.com>
2020-03-05 13:57:23 +0100

    Seeders actualizados (2)

---
+++ LibrosAumentadosApp/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('users')->delete();
+        
+        \DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 15,
+                'name' => 'a',
+                'email' => 'a@a.a',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$dOn/384cHUHX6.rufUz4ke1uDnzF7Il1.QtLBjRa.EeVyJmDowPSa',
+                'remember_token' => NULL,
+                'created_at' => '2020-02-19 11:45:06',
+                'updated_at' => '2020-02-19 11:45:06',
+            ),
+        ));
+        
+        
+    }
+}
