673d03ec00ec982afaf941b1a41b5f3df80bc459
jmado <josemaldogomez@gmail.com>
2020-03-11 09:54:11 +0100

    css

--- LibrosAumentadosApp/resources/views/libro/form.blade.php
+++ LibrosAumentadosApp/resources/views/libro/form.blade.php
@@ -3,56 +3,59 @@
 
 @section("content")
 
-<section class="text-center">
-    <div class="container">    
-        <button class="btn btn-primary btn-block" role="button" onclick="goBack()">Atras</button>
-        <script>
-            function goBack() {
-                window.history.back();
-            }
-        </script>
-        
+<div class="container p-5">
+    <section class="text-center">
+        <div class="container">    
+            <button class="btn btn-primary btn-block" role="button" onclick="goBack()">Atras</button>
+            <script>
+                function goBack() {
+                    window.history.back();
+                }
+            </script>
+            
+        </div>
+    </section>
+
+
+    <div class="container">
+            @isset($libro)
+            <form action="{{ route('libro.update', ['libro' => $libro->id]) }}" method="POST" enctype="multipart/form-data" class="formulario">
+            @method("PUT")
+            @else
+            <form action="{{ route('libro.store') }}" method="POST" enctype="multipart/form-data" class="formulario">
+            @endisset
+                @csrf
+
+                <div class="form-group">
+                    <label for="title">Titulo:</label>
+                    <input id="title" type="text" class="form-control" name="titulo" value="{{$libro->titulo ?? ''}}" required>
+                </div>
+                    
+                <div class="form-group">
+                    <label for="author">Autor:</label>  
+                    <input id="author" type="text" class="form-control" name="autor" value="{{$libro->autor ?? ''}}" required>
+                </div>  
+                
+                <div class="form-group">
+                    <label for="sub">Subtitulo:</label>
+                    <input id="sub" type="text" class="form-control" name="subtitulo" value="{{$libro->subtitulo ?? ''}}" required>
+                </div>  
+
+                <div class="custom-file">
+                        <input type="file" class="form-control" name="cubierta" class="custom-file-input" id="fichero" lang="es" required>
+                        <label class="custom-file-label" for="fichero">Seleccionar Imagen</label>
+                </div>  
+
+                    
+                <input type="submit" value="Enviar" class="btn btn-primary btn-block" role="button">
+
+            </form>
+                
+                
+            
     </div>
-</section>
-
-
-<div class="container">
-        @isset($libro)
-        <form action="{{ route('libro.update', ['libro' => $libro->id]) }}" method="POST" enctype="multipart/form-data" class="formulario">
-        @method("PUT")
-        @else
-        <form action="{{ route('libro.store') }}" method="POST" enctype="multipart/form-data" class="formulario">
-        @endisset
-            @csrf
-
-            <div class="form-group">
-                <label for="title">Titulo:</label>
-                <input id="title" type="text" class="form-control" name="titulo" value="{{$libro->titulo ?? ''}}" required>
-            </div>
-                
-            <div class="form-group">
-                <label for="author">Autor:</label>  
-                <input id="author" type="text" class="form-control" name="autor" value="{{$libro->autor ?? ''}}" required>
-            </div>  
-            
-            <div class="form-group">
-                <label for="sub">Subtitulo:</label>
-                <input id="sub" type="text" class="form-control" name="subtitulo" value="{{$libro->subtitulo ?? ''}}" required>
-            </div>  
-
-            <div class="custom-file">
-                    <input type="file" class="form-control" name="cubierta" class="custom-file-input" id="fichero" lang="es" required>
-                    <label class="custom-file-label" for="fichero">Seleccionar Imagen</label>
-            </div>  
-
-                
-            <input type="submit" value="Enviar" class="btn btn-primary btn-block" role="button">
-
-        </form>
-            
-            
-        
 </div>
+
 
 @endsection
 
