dba0c1f49fa5cff753cf3992f0baf41300620901
jmado <josemaldogomez@gmail.com>
2020-03-11 09:34:50 +0100

    Video show CSS

--- LibrosAumentadosApp/resources/views/video/show.blade.php
+++ LibrosAumentadosApp/resources/views/video/show.blade.php
@@ -3,27 +3,28 @@
 
 
 @section("content")
+<div class="container p-5">
+    <section class="text-center">
+      <div class="container">
+          <p>
+            <a href="{{ route('video.all', $datos->capitulo_id) }}" class="btn btn-primary " role="button">Videos</a>
+          </p>
+        </div>
+    </section>
 
-<section class="text-center">
-  <div class="container">
-      <p>
-        <a href="{{ route('video.all', $datos->capitulo_id) }}" class="btn btn-primary " role="button">Videos</a>
-      </p>
+
+    <div class="container"> 
+        <div class="row imagen">
+            <iframe src="https://player.vimeo.com/video/{{$videoNumero}}" width="640" height="361" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            <p><a href="{{$datos->video}}">{{$datos->titulo}}</a> disponible en <a href="https://vimeo.com">Vimeo</a>.</p> 
+        </div> 
+        <div class="row">
+            <h1>{{$datos->titulo}}</h1>
+            <a href="capitulo">Capitulo: {{$datos->capitulo_id}}</a>
+            <p class="lead">Descripcion: {{$datos->descripcion}}</p>   
+        </div>
     </div>
-</section>
+</div>
 
-
-<div class="container"> 
-    <div class="imagen">
-    <iframe src="https://player.vimeo.com/video/{{$videoNumero}}" width="640" height="361" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-    <p><a href="{{$datos->video}}">{{$datos->titulo}}</a> disponible en <a href="https://vimeo.com">Vimeo</a>.</p> 
-    </div> 
-    <div>
-      <h1>{{$datos->titulo}}</h1>
-      <a href="capitulo">Capitulo: {{$datos->capitulo_id}}</a>
-      <p class="lead">Descripcion: {{$datos->descripcion}}</p>
-     
-    </div>
-  </div>
 
 @endsection
