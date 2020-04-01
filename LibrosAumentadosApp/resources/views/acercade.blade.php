d3ca8b66da4333d79454591ae5bed0591806912b
Adrian Sanchez Gil <adsagil@gmail.com>
2020-04-01 11:48:29 +0200

    Acerca de

--- LibrosAumentadosApp/resources/views/acercade.blade.php
+++ LibrosAumentadosApp/resources/views/acercade.blade.php
@@ -1,6 +1,17 @@
 @extends("layouts.master")
 
 @section("content")
-Prueba
 
+<div class="acercade">
+    <p>Esta página web está diseñada para proporcionar un contenido multimedia extra a cualquier libro, 
+    desde imágenes o transparencias de imágenes hasta diseños 3D.</p>
+    <p>Esto permite al usuario una mayor introducción en el contenido del libro y así poder disfrutar de una 
+    manera de la que no se había llegado nunca hasta ahora.</p>
+    <p>A través de ella puedes conocer las historias como si de una propia situación se tratase, es decir,
+    desde el punto de vista del autor.</p>
+    <p>El desarrollo de esta Web corre a cargo de Jose Manuel Maldonado y Adrián Sánchez, 
+    con la supervisión de Alfredo Moreno y Félix para el pryecto final del curso 2019-2020 en el instituto I.E.S.
+    Celia Viñas (Almería)</p>
+
+</div>
 @endsection
