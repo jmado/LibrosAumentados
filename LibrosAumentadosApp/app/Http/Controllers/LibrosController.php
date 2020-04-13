fb48aac906713f02947ad1e250674d89ed61688e
Adrian Sanchez Gil <adsagil@gmail.com>
2020-04-13 19:49:10 +0200

    prueba loginVisitante5

--- LibrosAumentadosApp/app/Http/Controllers/LibrosController.php
+++ LibrosAumentadosApp/app/Http/Controllers/LibrosController.php
@@ -160,18 +160,10 @@ class LibrosController extends Controller
 
         // Después, elegimos una página al azar del capítulo seleccionado
         $paginas = DB::select("select * from paginas where capitulo_id=:id",['id'=>$capitulo->id]);   
-    
-        $numPagina = "";
-        if($numPagina < 0) {
-            $numPagina = rand(0, count($paginas)-1);
-        }else {
-            $numPagina = rand(0, count($paginas)-1);
-        }
-        echo "numPagina: $numPagina <br>";
+      
+        $numPagina = rand(0, count($paginas)-1);
         $numero_pagina = $paginas[$numPagina]->numero_pagina;
-        echo "numero_pagina: $numero_pagina <br>";
         $contenidoPagina = $paginas[$numPagina]->texto; 
-        echo "contenidoPagina: $contenidoPagina <br>";       
 
         // Ahora vamos a elegir un párrafo al azar de esa página.
         // No todos los párrafos valen, solo los que tengan más de 5 palabras.
