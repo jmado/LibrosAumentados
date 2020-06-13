171ebd9523687eebf7e640f7d93a63de6ebe76d3
jmado <josemaldogomez@gmail.com>
2020-06-13 17:53:53 +0200

    Buscador de libros en la vista de libros.all

--- LibrosAumentadosApp/resources/views/libro/all.blade.php
+++ LibrosAumentadosApp/resources/views/libro/all.blade.php
@@ -13,7 +13,23 @@
         <div class="row">
 
             <div class="col-md-3">
-                Buscador
+                <form class="search d-block">
+                    <input class="form-control libros-input" id="search" type="search" placeholder="Buscador de libros" aria-label="Search">
+                  </form>
+                <div class="search-libros">
+                    <ul class="libros-php">
+                      @forelse ($libroList as $l)
+                        <li>
+                          <a href="{{ route('contenido.contenido', $l->id) }}">
+                            {{$l->titulo}}
+                          </a>
+                        </li>
+                      @empty
+                        <li>...</li>
+                      @endforelse
+                    </ul>
+                    
+                </div>
             </div>
             @foreach ($libroList as $libro)   
             <div class="col-md-3 badge-secondary">
@@ -38,41 +54,54 @@
     </div>    
 </div>
 
-{{-- 
-<section class="text-center">
-    <div class="container-fluid">
-        <h1>Libros</h1>
-        
-    </div>
-</section>
+<script>
+            $(function(){
+                  //$('.search-libros').hide();
+                  
+                $.ajaxSetup({
+                    headers: {
+                      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                  
+                let tasks;
+                $('#search').keyup(function(){
+                    if($('#search').val()){
+                        let search = $('#search').val();
+                        console.log(search);
+                        $.ajax({
+                            url: "{{ route('contenido.libros') }}",
+                            type: 'POST',
+                            data: {search: search},
+                            success: function(response){
+                            
+                                tasks = JSON.parse(response);
+                                console.log(tasks);
+                                    let template = '';
+                                    
+                                    
+                                    tasks.forEach(task => {
 
-    <div class="elementos container">
-    {{ $libroList->links() }}
-        <div class="row">
+                                    var ruta = "{{ route('contenido.contenido', 1) }}";  
+                                    ruta = ruta.substr(0, ruta.length-1); 
+                                    var id = task.id;
+                                    var titulo = task.titulo;
+                                    
 
+                                    
+                                        template += "<ul class='libros-php'><li><a href='"+ruta+""+id+"'>"+titulo+"</li></a></ul>";
+                                    });
+                                    $('.search-libros').html(template);
+                                    $('.search-libros').show();
+                            }
+                        });
+                    }else{
+                        
+                    }
+                });
 
-            @foreach ($libroList as $libro)   
-            <div class="col-md-3">
-                <div class="elemento mb-4">
-                    <div class="elemento-header">
-                    <p>   
-                        <a href="{{route('contenido.contenido', $libro->id)}}"><img src='{{ URL::asset("$libro->cubierta")}}' class="cubierta"></a>
-                    </p>
-                    </div>
-                    <div class="elemento-body">
-                        <h6>{{$libro->titulo}}</h6>
-                        <p>{{$libro->subtitulo}}</p>
-                        <p>Autor: {{$libro->autor}}</p>
-                    </div>
-                </div>
-            </div>  
-            @endforeach
-
-
-        </div>
-       
-    </div>    
-    --}}
+            });   
+</script>
 
 
 
